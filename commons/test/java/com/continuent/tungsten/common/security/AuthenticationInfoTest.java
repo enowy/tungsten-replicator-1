99e820925044a379fd6bed9a2fff90688c211cad
Vilho Raatikka <raatikka@iki.fi>
2015-10-19 13:54:05 +0300

    CONT-1252
    AuthenticationInfoTest now allows for 0-9 cipher suites in sample.security.properties
    
    (cherry picked from commit 4e2d09365b9be0b54697f5c62d68ffecf0f8bf5e)
    Signed-off-by: Jeff Mace <jmace@vmware.com>

--- commons/test/java/com/continuent/tungsten/common/security/AuthenticationInfoTest.java
+++ commons/test/java/com/continuent/tungsten/common/security/AuthenticationInfoTest.java
@@ -98,4 +98,138 @@ public class AuthenticationInfoTest extends TestCase
         // Reset info
         SecurityHelperTest.resetSecuritySystemProperties();
     }
+
+    /**
+     * Confirm that when setting security.rmi.encryption=true and
+     * connector.security.use.ssl=false, we still have
+     * isEncryptionNeeded()=false TUC-1071
+     * 
+     * @throws ConfigurationException
+     */
+    public void testTUC1071()
+    {
+        // Reset info
+        SecurityHelperTest.resetSecuritySystemProperties();
+
+        // Confirm that exception is thrown when keystore location is not
+        // specified
+        AuthenticationInfo authInfo = null;
+        try
+        {
+            authInfo = SecurityHelper.loadAuthenticationInformation(
+                    "test.tuc1071.security.properties", true,
+                    TUNGSTEN_APPLICATION_NAME.CONNECTOR);
+
+            assertFalse(authInfo.isEncryptionNeeded());
+            assertFalse(authInfo.isConnectorUseSSL());
+        }
+        catch (ServerRuntimeException e)
+        {
+            assertTrue("There should not be any exception thrown", false);
+        }
+        catch (ConfigurationException e)
+        {
+            assertFalse(
+                    "That should not be this kind of Exception being thrown",
+                    true);
+        }
+
+        // Reset info
+        SecurityHelperTest.resetSecuritySystemProperties();
+    }
+
+    /**
+     * Confirm that trying to load an empty keystore will generate an error
+     * 
+     * @throws ConfigurationException
+     */
+    public void testTUC1080()
+    {
+        // Reset info
+        SecurityHelperTest.resetSecuritySystemProperties();
+
+        // Confirm that exception is thrown when keystore location is not
+        // specified
+        AuthenticationInfo authInfo = null;
+        try
+        {
+            authInfo = SecurityHelper.loadAuthenticationInformation(
+                    "test.ssl.alias.emptyKeystore.security.properties", true,
+                    TUNGSTEN_APPLICATION_NAME.ANY);
+            assertFalse(
+                    "The keystore is empty: An exception should have been thrown",
+                    true);
+        }
+        catch (ServerRuntimeException e)
+        {
+            assertTrue("There should not be any exception thrown", false);
+        }
+        catch (ConfigurationException e)
+        {
+            assertTrue("That's expected: the keystore is empty", true);
+        }
+
+        // Reset info
+        SecurityHelperTest.resetSecuritySystemProperties();
+    }
+
+    /**
+     * Confirm that we can retrieve enabled protocols and cipher suites
+     * 
+     * @throws ConfigurationException
+     */
+    public void testEnabledProtocolsAndCipherSuites()
+    {
+        // Reset info
+        SecurityHelperTest.resetSecuritySystemProperties();
+
+        // Confirm that exception is thrown when keystore location is not
+        // specified
+        AuthenticationInfo authInfo = null;
+        AuthenticationInfo authInfo_noInfo = null;
+        try
+        {
+            // Confirm that values can be read
+            authInfo = SecurityHelper.loadAuthenticationInformation(
+                    "sample.security.properties", true,
+                    TUNGSTEN_APPLICATION_NAME.ANY);
+            // File with no protocol or cipher info inside.
+            authInfo_noInfo = SecurityHelper.loadAuthenticationInformation(
+                    "test.ssl.alias.security.properties", true,
+                    TUNGSTEN_APPLICATION_NAME.ANY);
+            
+            List<String> listEnabledProtocols = authInfo.getEnabledProtocols();
+            assertNotNull("The list of Protocols should not be null", listEnabledProtocols);
+            assertFalse("The list of Protocols should contain protocols", listEnabledProtocols.isEmpty());
+            assertTrue("The list of Protocols should contain 2 protocols", listEnabledProtocols.size()==2);
+            
+            List<String> listEnabledCipherSuites = authInfo.getEnabledCipherSuites();
+            assertNotNull("The list of Cipher Suites should not be null", listEnabledCipherSuites);
+            assertFalse("The list of Cipher Suites should contain cipher suites", listEnabledCipherSuites.isEmpty());
+            // Length of VMware approved ciphers is 9 according to 
+            // https://wiki.eng.vmware.com/VSECR/vSDL/PSP/PSPRequirements#.C2.A0.C2.A0.5B3.3.E2.80.93M.5D_TLS_Cipher-Suites
+            assertTrue("The list of Cipher Suites should contain at most 9 cipher suites", listEnabledCipherSuites.size()<=9);
+            
+            // Confirm that empty properties return correct values
+            listEnabledProtocols = authInfo_noInfo.getEnabledProtocols();
+            assertNotNull("The list of Protocols should not be null", listEnabledProtocols);
+            assertTrue("The list of Protocols should be empty", listEnabledProtocols.isEmpty());
+            
+            listEnabledCipherSuites = authInfo_noInfo.getEnabledCipherSuites();
+            assertNotNull("The list of Cipher Suites should not be null", listEnabledCipherSuites);
+            assertTrue("The list of Cipher Suites should be empty", listEnabledCipherSuites.isEmpty());
+        }
+        catch (ServerRuntimeException e)
+        {
+            assertTrue("There should not be any exception thrown", false);
+        }
+        catch (ConfigurationException e)
+        {
+            assertTrue("There should not be any exception thrown", false);
+        }
+
+        // Reset info
+        SecurityHelperTest.resetSecuritySystemProperties();
+    }
+
 }
